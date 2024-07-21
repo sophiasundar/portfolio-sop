7ccfa6e0a9f14567ac8600d536c0763d5aaa5334
sophiasundar <sophiagifta17@gmail.com>
2024-07-21 23:53:45 +0530

    commit

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -1,57 +1,71 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
-import {toast} from 'react-toastify';
-
-
+import Textarea from '@mui/joy/Textarea';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
+import Button from '@mui/material/Button';
 
    export const Contact = ()=>{
 
-        const form = useRef();
+        const [name, setName] = useState('');
+        const [email,setEmail] = useState('');
+        const [message,setMessage] = useState('');
+        const [validated,setValidated] = useState(false);
 
         const handleSubmit = (e) =>{
             e.preventDefault();
 
-           
+            if(name===""){
+                setValidated("Name is required");
+                return;
+            }else if(email===""){
+                setValidated("Email is required and valid");
+                return;
+            }else if(message===""){
+                setValidated("Type Message");
+                return;
+            }else{
+                setValidated("")
+            }
+
+            const form = e.currentTarget;
+     if (form.checkValidity() === false) {
+       e.preventDefault();
+       e.stopPropagation();
+     }
+ 
+     setValidated(true);
+
+            const YOUR_SERVICE_ID = 'service_0fnz9yn';
+            const YOUR_TEMPLATE_ID = 'template_b4fmsjx';
+            const YOUR_PUBLIC_KEY = 'TMNtFZ0OMD-P10Fzp';
+
+
+            var templateParams = {
+                from_name: name,
+                from_email: email,
+                message: message,
+              };
         
 
         emailjs
-        .send(
-
-            'YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
-              publicKey: 'YOUR_PUBLIC_KEY',
+        .send(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, templateParams, {
+              publicKey: YOUR_PUBLIC_KEY,
            }
         )
-        .then((result)=>{
-            if(result.text === 'ok'){
-                toast.success('Your Message Sent Successfully 🙌🏽🎉✨ ',{
-                    position: "top-center",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "colored",
-                    backgroundColor: '#001F3F', 
-                    color: '#fff'
-                });
+        .then((response)=>{
+           
+             alert('Your Message Sent Successfully 🙌🏽🎉✨ ',response);
+            setName('')
+            setEmail('')
+            setMessage('')
             }
-        }).catch((error)=>{
+        ).catch((error)=>{
             console.log(error);
-            toast.error('Some Error Occured 😓', {
-                position: "top-center",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "colored",
-                backgroundColor: '#001F3F', 
-                    color: '#fff'
-            })
+            alert('Some Error Occured 😓')
         })
 
     }
@@ -67,28 +81,42 @@ import {toast} from 'react-toastify';
                    
                         <div >
                            
-                           
+                        
+                       
+                        <FormControl className='form'  onSubmit={handleSubmit}>
+                        <h6 className="svalid">{validated}</h6>
 
-                        <form ref={form} onSubmit={handleSubmit}>
-                           <label>Name</label>
-                            <input type="text" name="from_name" />
-                            <label>Email</label>
-                            <input type="email" name="email_id" />
-                            <label>Message</label>
-                            <textarea name="message" />
-                            <input type="submit" value="Send" />
-                        </form>
+                        <div>
+                        <TextField className='name' id="outlined-basic" label="Name" variant="outlined" 
+                           value={name}
+                           onChange={(e)=>{setName(e.target.value)}}
+                        
+                        />
+                        </div>
+
+                        <div className='emaildiv'>
+                        <TextField className='email' id="outlined-basic" label="Email" variant="outlined" 
+                        value={email}
+                        onChange={(e)=>{setEmail(e.target.value)}}
+                        
+                        />
+                        <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+                        
+                        </div>
+  
+                        <Textarea className='message' placeholder="Message:  Type anything…" 
+                            value={message}
+                            onChange={(e)=>{setMessage(e.target.value)}}
+                            
+                            />
+                           <Button className='contactbtn' type='submit' variant="contained">Submit</Button>
+                        </FormControl>
 
                      
 
                    </div>
                    </Box>
 
-                   {/* start */}
-
-                   
-
-                           {/* end */}
 
                 </Paper>
                   
